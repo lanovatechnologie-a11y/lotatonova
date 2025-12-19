48a3a996a84f4a9e145647afd81fcec79f0d15bb
Lota <lanovatechnologie@gmail.com>
2025-12-18 18:13:34 -0800

    Delete public directory

--- public/assets/notifications.js
+++
@@ -1,9 +0,0 @@
-function showNotification(message, type = 'info') {
-  const notification = document.createElement('div');
-  notification.className = `notification ${type}`;
-  notification.textContent = message;
-  document.body.appendChild(notification);
-  setTimeout(() => notification.remove(), 5000);
-}
-
-export { showNotification };

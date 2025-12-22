e8e01a6fc1ae32ed1422dc1ed20328b4ceb92fdb
Lota <lanovatechnologie@gmail.com>
2025-12-21 19:02:24 -0500

    Delete nova-lotto/src directory

--- nova-lotto/src/services/supabase-clients.js
+++
@@ -1,9 +0,0 @@
-// supabase-client.js
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
